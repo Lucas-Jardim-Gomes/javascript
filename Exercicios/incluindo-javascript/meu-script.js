9a12d714ed915d70f58f2503fdcec1b340bd5778
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2022-12-19 14:47:48 -0300

    Update meu-script.js

--- Exercicios/incluindo-javascript/meu-script.js
+++ Exercicios/incluindo-javascript/meu-script.js
@@ -1,2 +1,7 @@
-alert ("Ola seja bem vindo ao curso");
+//alert ("Ola seja bem vindo ao curso");
+
+/*
+    selecionar um elemento no DOM
+    atualizar o valor desse elemento com uma string
+*/
 document.getElementById("nome").value = "Oi" ;
