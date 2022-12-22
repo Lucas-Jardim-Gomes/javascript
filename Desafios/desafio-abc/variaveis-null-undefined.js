0f38af60d72170eaae69fab36321d9d4a5ada5ae
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2022-12-22 19:28:08 -0300

    desafio abc

---
+++ Desafios/desafio-abc/variaveis-null-undefined.js
@@ -0,0 +1,16 @@
+var a = 10;
+var b = 20;
+var c = null; //variavel auxiliar utilizada na lógica
+
+document.write("A: " + a + "<br />");
+document.write("B: " + b + "<br />");
+document.write("C: " + c + "<br />");
+
+document.write("<ht />")
+
+//lógica de inversão dos valores das variáveis A e B
+
+//
+document.write("A: " + a + "<br />");
+document.write("B: " + b + "<br />");
+document.write("C: " + c + "<br />");
