9f5d238393a72376e6438064813d014267ca98a0
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2022-12-23 18:45:43 -0300

    novo desafio

---
+++ Desafios/desafio-idade/desafio_idades.js
@@ -0,0 +1,33 @@
+var idade = prompt("Digite a sua idade");
+
+
+var criança = 15;
+var jovem = 30;
+var adulto = 60;
+var idoso = 60;
+
+if (idade <= criança ) {
+    document.write("criança")
+} else {
+    document.write("jovem")
+}
+
+
+
+/*
+var nota = prompt("Digite a nota do aluno:");
+var faltas = prompt("Digite a quantidade de faltas:");
+
+
+var media = 5;
+var faltas_maximas = 15
+
+if (nota >= media && faltas <= faltas_maximas) {
+    document.write("Aprovado");
+} else {
+    document.write("Reprovado");
+}
+*/
+
+
+
