891ae2ae2d0e0af82e12915919257229b8c943ab
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2022-12-23 09:46:52 -0300

    Update variaveis-null-undefined.js

--- Desafios/desafio-abc/variaveis-null-undefined.js
+++ Desafios/desafio-abc/variaveis-null-undefined.js
@@ -6,11 +6,23 @@ document.write("A: " + a + "<br />");
 document.write("B: " + b + "<br />");
 document.write("C: " + c + "<br />");
 
-document.write("<ht />")
+document.write("<hr />");
 
 //lógica de inversão dos valores das variáveis A e B
 
-//
-document.write("A: " + a + "<br />");
+var c = a;
+var a = b;
+var b = c;
+c = null;
+
+
+/* c = a //utilizada a variável C para armazenar temporariamente o valor da
+variável A
+ a = b //sobrepõe o valor da variável A com o valor da variável B
+ b = c //sobrepõe o valor da variável B com o valor da variável C (C nesse
+momento contém o valor da variável A)
+ c = null //opcional: remove o valor contido na variável C
+ */
+document.write("A: " + a +  "<br />");
 document.write("B: " + b + "<br />");
 document.write("C: " + c + "<br />");
