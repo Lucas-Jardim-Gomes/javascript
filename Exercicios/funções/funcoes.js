e92a012932f080cc4fa1bc45f51e43940045ae95
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2022-12-26 09:39:54 -0300

    novo exercício

---
+++ Exercicios/funções/funcoes.js
@@ -0,0 +1,16 @@
+
+//void
+//retorno
+
+function calcularAreaTerreno( largura, comprimento){
+    //lógica
+    var area = largura * comprimento
+    return area 
+}
+
+var largura = prompt("Dif
+")
+
+var area = calcularAreaTerreno(15, 25)
+
+document.write("O terreno possui " + area + " metros quadrados" )
