ff7a04eda69bd1548d29aa5cffd21f4dcc7068b6
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2022-12-26 12:32:53 -0300

    funções matemáticas e funções de string

---
+++ Exercicios/funções-matematicas/funcoes-matematicas.js
@@ -0,0 +1,12 @@
+var x = Math.ceil (10.390)
+document.write(x)
+document.write("<br>")
+var y = Math.floor (10.390)
+document.write(y)
+document.write("<br>")
+// o metodo math.round ele vai observar o valor da variavel e decidir se vai arredondar para cima ou para baixo
+var l = Math.round (10.490)
+document.write(l)
+document.write("<br>")
+var r = Math.random (10.390)
+document.write(r)
---
+++ funções-string/funcoes-strings.js
@@ -0,0 +1,21 @@
+//propriedade length
+
+var nome = " Lucas Jardim Gomes "
+
+document.write(nome.length)
+document.write("<br>")
+document.write(nome.charAt(11))
+document.write("<br>")
+document.write(nome.indexOf("m"))
+document.write("<br>")
+document.write(nome.replace("Lucas", "Gomes"))
+document.write("<br>")
+document.write(nome.substring(9, 3))
+document.write("<br>")
+document.write(nome.toLowerCase())
+document.write("<br>")
+document.write(nome.toUpperCase())
+document.write("<br>")
+document.write("-" + nome.trim() + "-")
+
+
