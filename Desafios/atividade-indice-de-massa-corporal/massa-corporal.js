5de693869d3f16e41b61fb593f15498e89d68b3c
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2022-12-26 09:14:10 -0300

    Update massa-corporal.js

--- Desafios/atividade-indice-de-massa-corporal/massa-corporal.js
+++ Desafios/atividade-indice-de-massa-corporal/massa-corporal.js
@@ -21,22 +21,22 @@ var classificacao = ""
 
 if(M < 16){
     classificacao = "Baixo peso muito grave"}
-if(M >= 16 && M <= 16,99){
+if(M >= 16 && M <= 16.99){
     classificacao = "Baixo peso grave"
 }
-if(M >= 17 && M <= 18,49){
+if(M >= 17 && M <= 18.49){
     classificacao = "Baixo peso"
 }
-if(M >= 18,50 && M >= 24,99){
+if(M >= 18.50 && M <= 24.99){
     classificacao = "Peso normal"
 }
-if(M >= 25 && M <= 29,99){
+if(M >= 25 && M <= 29.99){
     classificacao = "Sobrepeso"
 }
-if(M >= 30 && M <34,99){
+if(M >= 30 && M <34.99){
     classificacao = "Obesidade grau I"
 }
-if(M >= 35 && M <= 39,99){
+if(M >= 35 && M <= 39.99){
     classificacao = "Obesidade grau II"
 }if(M >= 40){
     classificacao = "Obesidade grau III"
