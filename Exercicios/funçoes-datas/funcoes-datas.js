922e02d403e4e2fdf8eac3eb2218796bfd6dde98
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2022-12-29 14:54:59 -0300

    Update funcoes-datas.js

--- Exercicios/funçoes-datas/funcoes-datas.js
+++ Exercicios/funçoes-datas/funcoes-datas.js
@@ -1,3 +1,5 @@
+
+/*
 var data = new Date()
 
 data.getDate()
@@ -30,5 +32,49 @@ document.write(data.toString())
 document.write("<hr>")
 data.setFullYear(data.getFullYear() + 1)
 document.write(data.toString())
+*/
+
+//  29/12/2022
+var data1 = new Date(2022, 11, 29)
+
+//  03/01/2023
+var data2 = new Date(2023, 0, 03)
+
+document.write(data1.toString())
+document.write("<br>")
+document.write(data2.toString())
+
+
+document.write("<hr>" + "<br>")
+//converter datas para algo que possamos calcular
+document.write(data1.getTime())
+document.write("<br>")
+document.write(data2.getTime())
+document.write("<br>")
+
+//1 de janeiro de 1970
+
+//encontrar a quantidade de milissegundos entre data1 e data2
+
+
+var milissegundosEntreDatas = data2.getTime() - data1.getTime()
+document.write(milissegundosEntreDatas)
+
+
+/*
+1 dia tem 24 horas, cada hora tem 60 minutos, cada segundo tem 1000 milissegundos
+
+pergunta: então quantos mlissegundos existem em um dia?
+*/
+var milissegundosPorDia = (1*24*60*60*1000)
+document.write("<br>")
+document.write("1 dia tem: " + milissegundosPorDia + " milissegundos")
+
+document.write("<br>")
+document.write(milissegundosEntreDatas / milissegundosPorDia)
+
+
+
+
 
 
