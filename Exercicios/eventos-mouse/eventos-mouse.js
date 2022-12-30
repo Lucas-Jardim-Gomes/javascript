2c06c8c7a4c2a4643023be089dac2ea0f6facbb9
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2022-12-30 18:04:46 -0300

    atualização

---
+++ Exercicios/eventos-mouse/eventos-mouse.js
@@ -0,0 +1,15 @@
+function acao() {
+    alert("Evento disparado")
+}
+function acao2() {
+    alert("Evento disparado 2")
+}
+function acao3() {
+    alert("Evento disparado 3")
+}
+function acao4() {
+    alert("Evento disparado 4")
+}
+function acao5() {
+    alert("Evento disparado 5")
+}
