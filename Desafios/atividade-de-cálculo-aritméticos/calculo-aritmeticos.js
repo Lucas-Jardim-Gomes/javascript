f21b695be4ecc118c88adaae2997017a9e48b620
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2022-12-30 12:12:39 -0300

    calculo aritmético

---
+++ Desafios/atividade-de-cálculo-aritméticos/calculo-aritmeticos.js
@@ -0,0 +1,24 @@
+var num1 = prompt("Digite um numero: ")
+var operacao = prompt("Escreva soma ou subtração para prosseguir com a operação")
+var num2 = prompt("Digite um numero: ")
+
+
+function calculo (num1, operacao , num2){
+    num1 = parseFloat (num1)
+    num2 = parseFloat (num2)
+
+    var resultado = null
+
+    if(operacao == "soma"){
+        resultado = num1 + num2
+    }
+    if(operacao == "subtração"){
+        resultado = num1 - num2
+    }
+    
+    return resultado;
+}
+
+
+document.write("O resultado é: " + calculo (num1, operacao , num2))
+//document.write("O resultado é: " + resultado + ".")
