cc529d4e0d5e7143576a1aa1bfd3fbeba60a5c54
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2023-02-03 09:37:05 -0300

    estudo do DOM parte 4

---
+++ Exercicios/dom-manipulando-estilos-de-elementos/manipulando-estilo.js
@@ -0,0 +1,6 @@
+function modificaEstilo(corDeFundo, H, W) {
+
+    document.getElementById('retangulo').style.backgroundColor = corDeFundo
+    document.getElementById('retangulo').style.width = W
+    document.getElementById('retangulo').style.height = H
+}
