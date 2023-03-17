ea15ef4aed69dff0258dacac8937906b35882177
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2023-03-16 21:36:44 -0300

    exercício de Array basico

---
+++ Exercicios/array-basico.html/script.js
@@ -0,0 +1,19 @@
+// para escrever Array de outra forma temos aqui um exemplo:
+//var lista_frutas = Array()
+var lista_frutas = []
+lista_frutas[1] = "Kiwi"
+lista_frutas[2] = "Abacate"
+lista_frutas[3] = "Banana"
+lista_frutas[4] = "Ameixa"
+lista_frutas[5] = "Maçã"
+
+console.log(lista_frutas)
+
+/*para escrever Array de outra forma temos aqui um exemplo:
+
+var lista_frutas = Array("Banana", "Maçã", "Kiwi", "Abacate", "Ameixa")
+
+
+var lista_frutas = ["Banana", "Maçã", "Kiwi", "Abacate", "Ameixa"]
+
+*/
