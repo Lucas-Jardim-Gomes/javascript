3bb956b3105f3d8e0864121511b8e41b44c6294d
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2023-03-17 19:07:18 -0300

    Array Multidimensional

---
+++ Exercicios/Array-Multidimensional/script.js
@@ -0,0 +1,14 @@
+var lista_coisas = Array()
+        lista_coisas["frutas"] = Array()
+
+          lista_coisas["frutas"][1] = "Banana"
+          lista_coisas["frutas"][2] = "Abacate"
+          lista_coisas["frutas"][3] = "uva"
+
+        lista_coisas["pessoas"] = []
+
+          lista_coisas["pessoas"]['a'] = "Talita"
+          lista_coisas["pessoas"]['b'] = "Marcos"
+          lista_coisas["pessoas"]['c'] = "Ana"
+
+    document.write(lista_coisas["pessoas"]['c'])
