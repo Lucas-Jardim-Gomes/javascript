2abbe76455051ce3babb3ac21320ba05f304d954
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2023-03-20 08:44:11 -0300

    Update script.js

---
+++ Exercicios/array-incluir-remover/script.js
@@ -0,0 +1,31 @@
+var lista_frutas = Array()
+    lista_frutas[0] = 'Banana'
+    lista_frutas[1] = 'Abacate'
+    lista_frutas[2] = 'Uva'
+
+    // Incluir elemento no final do array
+    lista_frutas.push('Ameixa')
+
+    // Incluir elemento no inicio do array
+    lista_frutas.unshift('Manga')
+
+    // Excluir elemento no inicio do array
+    lista_frutas.shift()
+    // Excluir elemento no final do array
+    lista_frutas.pop()
+
+
+
+
+var lista_coisas = Array()
+    lista_coisas['pessoas'] = Array()
+    lista_coisas['pessoas'][0] = 'Lucas'
+    lista_coisas['pessoas'][1] = 'Mateus'
+
+    lista_coisas['objetos'] = []
+    lista_coisas['objetos'][0] = 'Cadeira'
+    lista_coisas['objetos'][1] = 'Copo'
+
+    lista_coisas['objetos'].push('Mesa')
+
+      console.log(lista_coisas)
