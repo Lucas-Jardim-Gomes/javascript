2bae9702dc36ccb02faeea78e769b9932e53f64f
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2023-03-22 09:07:39 -0300

    adicionei  o onclick no HTML / no JS criei function para recuperar o valor  digitado e algumas verificações com if e else

--- Desafios/atividade-ordenar-e-adicionar/script.js
+++ Desafios/atividade-ordenar-e-adicionar/script.js
@@ -1,11 +1,18 @@
-    var objetos = Array()
+function valorRecuperado(){
+    var caracter = (document.getElementById("entrada").value)
+
+    caracter.trim()
+
+
+     if (caracter === ""){
+        alert("Informe um valor valido")
+     } else {
+        caracter = objetos
+     }
+}
+
+
+ var objetos = Array()
         objetos[0] = 'Cadeira'
         objetos[1] = 'Impressora'
         objetos[2] = 'Garfo'
-
-function recuperarTexto(){
-    document.getElementById("campoDigitado").value
-}
-var resultado = recuperarTexto
-
-console.log("resultado")
