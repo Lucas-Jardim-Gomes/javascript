20ad40b2237e2bdc1d168c62cf4327ac775622ac
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2023-03-22 12:15:32 -0300

    Update script.js

--- Desafios/atividade-ordenar-e-adicionar/script.js
+++ Desafios/atividade-ordenar-e-adicionar/script.js
@@ -4,13 +4,16 @@ function valorRecuperado(){
     caracter.trim()
 
 
-     if (caracter === ""){
+     if (caracter = ""){
         alert("Informe um valor valido")
-     } else {
-        caracter = objetos
-     }
+     } if (caracter = objetos[""]) {
+        alert("Objeto já foi adicionado")
+     } else if (caracter != objetos) {
+        objetos.push(caracter)
+     } 
 }
 
+//lista_coisas['objetos'].push('Mesa')
 
  var objetos = Array()
         objetos[0] = 'Cadeira'
