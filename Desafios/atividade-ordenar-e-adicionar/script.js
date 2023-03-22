4723da6ba69751aeabd0a7e479c0273c6c53490d
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2023-03-21 22:04:45 -0300

    criei um novo desafio e fiz o input e os botões

---
+++ Desafios/atividade-ordenar-e-adicionar/script.js
@@ -0,0 +1,11 @@
+    var objetos = Array()
+        objetos[0] = 'Cadeira'
+        objetos[1] = 'Impressora'
+        objetos[2] = 'Garfo'
+
+function recuperarTexto(){
+    document.getElementById("campoDigitado").value
+}
+var resultado = recuperarTexto
+
+console.log("resultado")
