579bb7212875142869ea84fffd1b33e1c3f6eb1e
Lucasdev07 <113186715+Lucas-Jardim-Gomes@users.noreply.github.com>
2023-03-30 18:56:22 -0300

    Create casting.js

---
+++ Exercicios/casting/casting.js
@@ -0,0 +1,18 @@
+var variavel1 = 10
+var variavel2 = 20
+
+/*
+var variavel1 = prompt("Digite algum número");
+var variavel2 = prompt("Digite outro número");
+
+parseInt -> remove a parte da fração e preserva a parte inteira do número
+parseFloat -> preserva a fração
+
+variavel1 = parseInt(variavel1);
+variavel2 = parseFloat(variavel2);
+
+document.write(variavel1 + variavel2);
+
+*/
+
+document.write(variavel1.toString() + variavel2.toString());
