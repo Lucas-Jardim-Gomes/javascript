7eb315de3f961b0e1ba6ca21030164c31369ca31
Lucasdev07 <113186715+Lucas-Jardim-Gomes@users.noreply.github.com>
2023-03-30 16:08:39 -0300

    Create script.js
    aqui utilizei o window.screen para obter a dimensão da atura  e largura da tela, depois eu criei uma condição para tomar uma ação

---
+++ Exercicios/bom-screen/script.js
@@ -0,0 +1,10 @@
+var altura = window.screen.availHeight
+var largura = window.screen.availWidth
+
+//document.write('Altura do navegador é: ' + altura + '<br/>')
+//document.write('Largura do navegador é: ' + largura)
+if(largura -= 500){
+    document.write('Lógica para impressão do menu versaão mobile')
+} else {
+    document.write('Lógica para impressão do menu versaão web')
+}
